a2f9690f89d030e126564a15f2033fb66f8f8fca
Oleg Belousov <ol.jkl88@gmail.com>
2025-11-14 18:09:17 +0300

    v1.0.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ExportDXF_Kompas")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SkyKraft")]
 [assembly: AssemblyProduct("ExportDXF_Kompas")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
